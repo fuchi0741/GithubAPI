659b9526c670990cfb636d84fb23b5ccdd1653c1
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-03-23 13:45:31 +0900

    Initial Commit

---
+++ section18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  section18
+//
+//  Created by 渕一真 on 2021/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
